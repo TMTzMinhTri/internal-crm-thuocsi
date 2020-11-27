5c4c13b4e85b35e2d8067a8d3c652132f6c946ec
thuocsi.tamnt <tamth.nguyen@thuocsi.vn>
2020-11-27 16:41:59 +0700

    TEST DEV PROXY

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ module.exports = withPlugins([withTM], {
     images: {
         // domains: ['miro.medium.com'],
     },
-    assetPrefix: process.env.ENV==='local'?'/':'/crm',
-    useFileSystemPublicRoutes: true,
+    // assetPrefix: process.env.ENV==='local'?'/':'/crm',
+    // useFileSystemPublicRoutes: true,
     env: {
         WEB_HOST: process.env.WEB_HOST
     },
