66008954a91a966f2de273468bc98048e6bd2cc4
thuocsi.thanh.le <thanh.le@thuocsi.vn>
2021-03-02 14:20:30 +0700

    Fix redirect page

--- pages/crm/index.js
+++ pages/crm/index.js
@@ -1,11 +1,12 @@
-import { doWithLoggedInUser } from "@thuocsi/nextjs-components/lib/login";
-import PricingPage, { loadPricingData } from "pages/crm/sku/index";
-export async function getServerSideProps(ctx) {
-    return await doWithLoggedInUser(ctx, (ctx) => {
-        return loadPricingData(ctx)
-    })
+export function getServerSideProps() {
+    return {
+        redirect: {
+            permanent: true,
+            destination: "/crm/sku/active",
+        }
+    };
 }
 
-export default function HRMIndexPage(props) {
-    return PricingPage(props)
+export default function CRMIndexPage() {
+    return null;
 }
