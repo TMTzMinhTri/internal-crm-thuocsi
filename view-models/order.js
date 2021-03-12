7db69fb2535641bfc1326efeb2fd0c7c746aade9
thuocsi.thanh.le <thanh.le@thuocsi.vn>
2021-03-12 17:10:11 +0700

    Refactor order form

--- view-models/order.js
+++ view-models/order.js
@@ -1,3 +1,9 @@
 export const OrderPaymentMethod = {
     PAYMENT_METHOD_BANK: "PAYMENT_METHOD_BANK",
 }
+
+export const OrderStatus = {
+    WAIT_CONFIRM: "WaitConfirm",
+	CONFIRMED: "Confirmed",
+	CANCELED: "Canceled",
+}
